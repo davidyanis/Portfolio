cced715e804de6027a82b7466d16faf67b05520f
David Yan <daveddd97@hotmail.com>
2018-10-01 10:03:19 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function menuButtonClicked() {
+    var header = document.getElementById("header");
+    header.classList.toggle("fullscreen")
+ }
+
+function scrollToTop() {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+ 
+ 
